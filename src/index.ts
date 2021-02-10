67530cd1b55d821082abe72d7168261dac0a9a52
Ronye Vernaes <ronye.vernaes@hitachivantara.com>
2021-02-10 17:40:18 +0000

    Restore panel state

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import {
+  ILayoutRestorer,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { 
+  ICommandPalette,
+  MainAreaWidget,
+  WidgetTracker
+} from '@jupyterlab/apputils';
 
 import { APODWidget } from './APODWidget';
 
@@ -13,33 +18,50 @@ import { APODWidget } from './APODWidget';
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod:plugin',
   autoStart: true,
-  requires: [ICommandPalette],
+  requires: [ICommandPalette, ILayoutRestorer],
   activate: activate
 };
 
 
-function activate(app: JupyterFrontEnd, palette: ICommandPalette) {
+function activate(
+  app: JupyterFrontEnd,
+  palette: ICommandPalette,
+  restorer: ILayoutRestorer
+) {
   console.log('JupyterLab extension jupyterlab_apod is activated!');
+
+  let widget: MainAreaWidget<APODWidget>;
 
   const command: string = 'apod:open';
   app.commands.addCommand(command, {
     label: 'Random Astronomy Picture',
     execute: async () => {
-      const content = new APODWidget();
-      const widget = new MainAreaWidget({ content });
-      widget.id = 'apod-jupiterlab';
-      widget.title.label = 'Astronomy Picture';
-      widget.title.closable = true;
-
+      if (!widget || widget.isDisposed) {
+        const content = new APODWidget();
+        widget = new MainAreaWidget({ content });
+        widget.id = 'apod-jupiterlab';
+        widget.title.label = 'Astronomy Picture';
+        widget.title.closable = true;
+      }
+      if (!tracker.has(widget)) {
+        tracker.add(widget);
+      }
       if (!widget.isAttached) {
         app.shell.add(widget, 'main');
       }
-      content.update();
+      widget.content.update();
       app.shell.activateById(widget.id);
     }
   });
 
   palette.addItem({ command, category: 'Tutorial' });
+  let tracker = new WidgetTracker<MainAreaWidget<APODWidget>>({
+    namespace: 'apod'
+  });
+  restorer.restore(tracker, {
+    command,
+    name: () => 'apod'
+  });
 }
 
 export default extension;
